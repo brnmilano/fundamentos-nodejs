aacbaa29042bf744b8cb8b89767a97a25734c009
Bruno <brn.milano@hotmail.com>
2024-10-29 21:01:28 -0300

    Estrutura da aplicação: - Rotas de criação; - Salvando usuários em menória (headers) - Conhecendo HTTP status codes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 // Importação do HTTP do Node.js (módulo nativo)
 import http from "http";
 
+const users = [];
+
 const server = http.createServer((request, response) => {
   /**
    * Request - Consigo obter todas as informações da requisião que está
@@ -10,9 +12,27 @@ const server = http.createServer((request, response) => {
    * a requisição.
    */
 
-  return response.end("Hello World");
+  const { method, url } = request;
+
+  if (method === "GET" && url === "/users") {
+    return response
+      .setHeader("Content-Type", "application/json")
+      .end(JSON.stringify(users));
+  }
+
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "Bruno Milano",
+      email: "brnmilano@hotmail.com",
+    });
+
+    return response.writeHead(201).end();
+  }
+
+  return response.writeHead(404).end();
 });
 
 // A aplicação irá escutar a porta 3333.
 // localhost:3333
-server.listen(3000);
+server.listen(3333);
